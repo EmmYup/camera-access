45b4128d08c8f65022ea3771ed6985727505ba24
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-09-26 16:10:44 -0500

    more functions deleted

--- js/main.js
+++ js/main.js
@@ -17,8 +17,8 @@ $(function() {
             return self.handleError(err);
           }
           //Quagga.registerResultCollector(resultCollector);
-          App.attachListeners();
-          App.checkCapabilities();
+          App.initCameraSelection();
+
           Quagga.start();
         });
       });
@@ -26,40 +26,7 @@ $(function() {
     handleError: function(err) {
       console.log(err);
     },
-    checkCapabilities: function() {
-      var track = Quagga.CameraAccess.getActiveTrack();
-      var capabilities = {};
-      if (typeof track.getCapabilities === 'function') {
-        capabilities = track.getCapabilities();
-      }
-      this.applySettingsVisibility('zoom', capabilities.zoom);
-      this.applySettingsVisibility('torch', capabilities.torch);
-    },
-    applySettingsVisibility: function(setting, capability) {
-      // depending on type of capability
-      if (typeof capability === 'boolean') {
-        var node = document.querySelector(
-          'input[name="settings_' + setting + '"]'
-        );
-        if (node) {
-          node.parentNode.style.display = capability ? 'block' : 'none';
-        }
-        return;
-      }
-      if (
-        window.MediaSettingsRange &&
-        capability instanceof window.MediaSettingsRange
-      ) {
-        var node = document.querySelector(
-          'select[name="settings_' + setting + '"]'
-        );
-        if (node) {
-          this.updateOptionsForMediaRange(node, capability);
-          node.parentNode.style.display = 'block';
-        }
-        return;
-      }
-    },
+
     initCameraSelection: function() {
       var streamLabel = Quagga.CameraAccess.getActiveStreamLabel();
       return Quagga.CameraAccess.enumerateVideoDevices().then(function(
@@ -71,19 +38,6 @@ $(function() {
           $option.selected = streamLabel === device.label;
         });
       });
-    },
-    attachListeners: function() {
-      var self = this;
-      self.initCameraSelection();
-    },
-
-    setState: function(path, value) {
-      var self = this;
-
-      console.log(JSON.stringify(self.state));
-      App.detachListeners();
-      Quagga.stop();
-      App.init();
     },
     state: function(camera) {
       var state = {
